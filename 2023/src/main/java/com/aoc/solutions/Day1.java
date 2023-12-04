3bc92d190c846d401abcb233bc99b31ba874844e
jonkspanda <jionte@gmail.com>
2023-12-04 23:00:58 +0100

    removed print-lines

--- 2023/src/main/java/com/aoc/solutions/Day1.java
+++ 2023/src/main/java/com/aoc/solutions/Day1.java
@@ -39,7 +39,6 @@ public class Day1 extends Solution {
 
         for (String row : this.input.getRowsList()) {
             if (!(row.matches("^[0-9].*") && row.matches(".*[0-9]$")) && row.length() >= 3) {
-                System.out.println(row);
                 row = row.replaceAll("oneight", "oneeight");
                 row = row.replaceAll("fiveight", "fiveeight");
                 row = row.replaceAll("eightwo", "eighttwo");
@@ -52,18 +51,14 @@ public class Day1 extends Solution {
                 for (String numb : spelledNumbs) {
                     do {
                         row = row.replaceFirst(numb, Integer.toString(spelledNumbs.indexOf(numb) + 1));
-                        System.out.println(row);
                     } while (row.contains(numb));
                 }
             }
 
             // remove letters from String
             row = row.replaceAll("[^0-9]", "");
-            System.out.println(row);
             String first = row.substring(0, 1);
             String last = row.substring(row.length() - 1);
-            System.out.println(first);
-            System.out.println(last);
             int number = Integer.parseInt(first + last);
             sum += number;
         }
